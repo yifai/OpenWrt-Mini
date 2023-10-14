a208eb8b426db5facf0714551279049facf0d4d3
Bigdog <134132915+Bigdog-007@users.noreply.github.com>
2023-10-14 19:27:23 +0800

    B.sh

--- B.sh
+++ B.sh
@@ -1,5 +1,5 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
 git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
 git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
-sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
