ecf63874586394ab2b6556696120843105e233eb
Bigdog <134132915+Bigdog-007@users.noreply.github.com>
2023-10-14 13:11:00 +0800

    Add files via upload

---
+++ B.sh
@@ -0,0 +1,5 @@
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+sed -i '1i src-git haibo https://github.com/haiibo/openwrt-packages' feeds.conf.default
