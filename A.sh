2966a7740a49d619976946ee106f315ce8b722b6
Bigdog <134132915+Bigdog-007@users.noreply.github.com>
2023-10-22 21:31:16 +0800

    Delete A.sh

--- A.sh
+++
@@ -1,4 +0,0 @@
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/luci-app-poweroff
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
